575d92dedb59d704d360c4c2d4ee08237a4ebcdd
Mackgame4 <42896437+Mackgame4@users.noreply.github.com>
2019-03-02 16:40:19 +0000

    Add files via upload

---
+++ esx_billing/locales/pt.lua
@@ -0,0 +1,12 @@
+Locales['pt'] = {
+  ['invoices'] = 'Finanças',
+  ['invoices_item'] = '$%s',
+  ['received_invoice'] = 'Você ~r~recebeu~s~ uma fatura',
+  ['paid_invoice'] = 'Você ~g~pagou~s~ uma fatura nas finanças de ~r~$%s~s~',
+  ['received_payment'] = 'Você ~g~recebeu~s~ um pagamento de ~r~$%s~s~',
+  ['player_not_online'] = 'O jogador não está logado',
+  ['no_money'] = 'Não tens dinheiro suficiente',
+  ['target_no_money'] = 'O player ~r~nao tem~s~ dinheiro suficiente!',
+  ['financas'] = 'Finanças',
+  ['access_fincancas'] = '[E] para abrir o menu',
+}
